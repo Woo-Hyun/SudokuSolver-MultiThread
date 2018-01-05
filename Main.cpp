65b10196ca42c826e92c324405931d6d8fd541ff
WooHyun <dngusmoon@gmail.com>
2018-01-05 21:06:10 +0900

    add Main.cpp comments and change source code little.

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,29 @@
+/*************************************************************
+ * This is code written by Woo Hyun.
+ * SoongSil University, Smart Systems Software department.
+ * 06 / 10 / 2017 Proto type written.
+ * 
+ * This code is for solving sudoku puzzle.
+ * And it will return True or False text.
+ * 
+ * You must need *.txt for this program like this,
+ * 123456789
+ * 382094029
+ * 382904729
+ * 283764658
+ * 017305724
+ * 957201938
+ * 572301928
+ * 573920194
+ * 482014572
+ * Of course, this is WRONG sudoku canvas. Just example.
+ * 
+ * And, this code has modern C++ syntax and basic multi-threading.
+ * So, you need g++ option that -std=c++11 and -pthread.
+ * 
+ * Thank you for watching this code.
+ *************************************************************/
+
 #include "BoardMaker.hpp"
 #include "SudokuSolver.hpp"
 #include <iostream>
--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -6,8 +6,8 @@ using namespace std;
 const bool SudokuSolver::check_square(const int& rows, const int& cols) const {
     int** check = board_ptr->sudoku_board;
     int saved[10] = { 0 };
-    for (int i = rows; i < rows + 3; ++i) {
-	for (int j = cols; j < cols + 3; ++j) {
+    for (auto i = rows; i < rows + 3; ++i) {
+	for (auto j = cols; j < cols + 3; ++j) {
 	    int val = check[i][j];
 	    if (saved[val] != 0) {
 		return false;
@@ -22,9 +22,9 @@ const bool SudokuSolver::check_square(const int& rows, const int& cols) const {
 
 const bool SudokuSolver::run_cols() const {
     int** check = board_ptr->sudoku_board;
-    for (int i = 0; i < 9; ++i) {
+    for (auto i = 0; i < 9; ++i) {
 	int col[10] = { 0 };
-	for (int j = 0; j < 9; ++j) {
+	for (auto j = 0; j < 9; ++j) {
 	    int val = check[j][i];
 	    if (col[val] != 0) {
 		return false;
@@ -39,9 +39,9 @@ const bool SudokuSolver::run_cols() const {
 
 const bool SudokuSolver::run_rows() const {
     int** check = board_ptr->sudoku_board;
-    for (int i = 0; i < 9; ++i) {
+    for (auto i = 0; i < 9; ++i) {
 	int row[10] = { 0 };
-	for (int j = 0; j < 9; ++j) {
+	for (auto j = 0; j < 9; ++j) {
 	    int val = check[i][j];
 	    if (row[val] != 0) {
 		return false;
